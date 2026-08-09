4dce7c750fb27cc77f17f434938f761e837d6d47
Sudeep <sudeep@DESKTOP-UL44FOD.localdomain>
2026-08-09 20:50:41 +0000

    Integrate monitoring modules into dashboard

--- dashboard.sh
+++ dashboard.sh
@@ -1,11 +1,39 @@
 #!/bin/bash
 
-echo "==============================================================="
-echo "                           Linux System Monitoring Dashboard   "
-echo "==============================================================="
+SCRIPT_DIR="$(cd "$(dirname "${BASE_SOURCE[0]}")" && pwd)"
+MODULE_DIR="$SCRIPT_DIR/modules"
 
+clear
+
+echo "====================================================================================================="
+echo "                           Linux System Monitoring Dashboard   "
+echo "====================================================================================================="
+
+echo ""
 echo "Hostname : $(hostname)"
 echo "User     : $(whoami)"
 echo "Date     : $(date)"
+echo ""
 
-echo "==============================================================="
+echo ""
+"$MODULE_DIR/cpu.sh"
+
+echo ""
+"$MODULE_DIR/memory.sh"
+
+echo ""
+"$MODULE_DIR/disk.sh"
+
+echo ""
+"$MODULE_DIR/process.sh"
+
+echo""
+"$MODULE_DIR/network.sh"
+
+echo ""
+"$MODULE_DIR/system.sh"
+
+echo""
+echo "====================================================================================================="
+
+
